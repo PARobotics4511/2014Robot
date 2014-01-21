c88d24b9db26f05851d13212064f08d4dc52aa86
Theristis <toml960330@gmail.com>
2014-01-21 16:29:41 -0600

    Added basic controller movement commands
    c0nn3r iz 4m4z1n9

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -8,20 +8,35 @@ class RobotDemo : public SimpleRobot
 public:
 	RobotDemo() : notLogitech(1), CIMeon() {
 		CIMeon.cComponents.DriveTrain.SetExpiration(0.1);
+		CIMeon.cComponents.DriveTrain.SetSafetyEnabled(true);
 	}
 
 	void Autonomous()
 	{
-		CIMeon.cComponents.DriveTrain.SetSafetyEnabled(true);
 		//Do nothing for now
 	}
 
 	void OperatorControl()
 	{
-		CIMeon.cComponents.DriveTrain.SetSafetyEnabled(true);
+		float deadzone_y = notLogitech.GetX();
+		float deadzone_x = -notLogitech.GetY();
+		float deadzone_z = notLogitech.GetRawAxis(4)/3.0;
 		while (IsOperatorControl())
 		{
-			CIMeon.cComponents.DriveTrain.MecanumDrive_Cartesian(notLogitech.GetX(),notLogitech.GetY(),notLogitech.GetRawAxis(4)); //Drive with the motors on channels 4,5,6,7.  The arguments are x, y, direction, and not useful
+			deadzone_x = -notLogitech.GetY();
+			deadzone_y = notLogitech.GetX();
+			deadzone_z = notLogitech.GetRawAxis(4)/3.0;
+			
+			if (pow((deadzone_x*deadzone_x+deadzone_y*deadzone_y),0.5) < 0.15) {
+				deadzone_x = 0;
+				deadzone_y = 0;
+			}
+			if (abs(deadzone_z) < 0.15) {
+				deadzone_z = 0;
+			}
+			
+			CIMeon.cComponents.DriveTrain.MecanumDrive_Cartesian(deadzone_x, deadzone_y, deadzone_z+0.025); //Drive with the motors on channels 4,5,6,7.  The arguments are x, y, direction, and not useful
+			
 			Wait(0.005);
 		}
 	}
